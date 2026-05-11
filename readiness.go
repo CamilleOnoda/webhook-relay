4765c40a2fe140ea0280b21a0c1208551759adad
Camille Onoda <info@camilleonoda.com>
2026-05-11 17:27:03 +0900

    feat: add static app page routing and readiness status response

--- readiness.go
+++ readiness.go
@@ -4,8 +4,15 @@ import (
 	"net/http"
 )
 
+type readinessResponse struct {
+	Status string `json:"status"`
+}
+
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/plain")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+	res := readinessResponse{
+		Status: "ok",
+	}
+	w.Write([]byte(res.Status))
 }
