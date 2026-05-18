eeb92629ba951cd32bfaee2ab0bcf9ab530e4921
Camille Onoda <info@camilleonoda.com>
2026-05-18 17:51:35 +0900

    security: prevent forwarding of unsafe hop-by-hop headers

---
+++ internal/service/helper_unsafeHeaders.go
@@ -0,0 +1,20 @@
+package service
+
+func isUnsafeHeader(headerKey string) bool {
+	var unsfafeHeaders = map[string]bool{
+		"Host":                true,
+		"Content-Length":      true,
+		"Transfer-Encoding":   true,
+		"Connection":          true,
+		"Keep-Alive":          true,
+		"Proxy-Authenticate":  true,
+		"Proxy-Authorization": true,
+		"TE":                  true,
+		"Trailer":             true,
+		"Upgrade":             true,
+		"Cookie":              true,
+		"Set-Cookie":          true,
+		"Authorization":       true,
+	}
+	return unsfafeHeaders[headerKey]
+}
