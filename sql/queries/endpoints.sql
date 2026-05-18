d784bc4bcacd44aaf7d477d177a6c93cf728be77
Camille Onoda <info@camilleonoda.com>
2026-05-18 11:14:51 +0900

    feat: add queries to ListEndpoints GetEndpointsByID DeleteAllEndpoints and DeleteEndpointsByID

--- sql/queries/endpoints.sql
+++ sql/queries/endpoints.sql
@@ -2,3 +2,18 @@
 INSERT INTO webhook_endpoints (name, target_url)
 VALUES ($1, $2)
 RETURNING *;
+
+-- name: ListEndpoints :many
+SELECT * FROM webhook_endpoints
+ORDER BY created_at DESC;
+
+-- name: GetEndpointByID :one
+SELECT * FROM webhook_endpoints
+WHERE id = $1;
+
+-- name: DeleteAllEndpoints :exec
+DELETE FROM webhook_endpoints;
+
+-- name: DeleteEndpointByID :exec
+DELETE FROM webhook_endpoints
+WHERE id = $1;
