dcb44bc72d93f4243574bcc29569c67d52eb4e41
Camille Onoda <info@camilleonoda.com>
2026-05-18 17:50:53 +0900

    feat(db): add endpoint description field and update query support

--- sql/queries/endpoints.sql
+++ sql/queries/endpoints.sql
@@ -1,6 +1,6 @@
 -- name: CreateEndpoint :one
-INSERT INTO webhook_endpoints (name, target_url)
-VALUES ($1, $2)
+INSERT INTO webhook_endpoints (name, target_url, description)
+VALUES ($1, $2, $3)
 RETURNING *;
 
 -- name: ListEndpoints :many
@@ -17,3 +17,9 @@ DELETE FROM webhook_endpoints;
 -- name: DeleteEndpointByID :exec
 DELETE FROM webhook_endpoints
 WHERE id = $1;
+
+-- name: UpdateEndpoint :one
+UPDATE webhook_endpoints
+SET name = $2, target_url = $3, description = $4
+WHERE id = $1
+RETURNING *;
