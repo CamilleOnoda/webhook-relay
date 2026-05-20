76918348b1597d79dd962eddec1ff456619dbc27
Camille Onoda <info@camilleonoda.com>
2026-05-20 13:20:54 +0900

    refactor(events): add dedicated Event API response struct for clear separation between DB model (database.WebhookEvent) and API response model (Event)

--- handler_events_get.go
+++ handler_events_get.go
@@ -2,23 +2,50 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/CamilleOnoda/webhook-relay.git/internal/database"
+	"github.com/google/uuid"
 )
+
+type Event struct {
+	ID           uuid.UUID `json:"id"`
+	EndpointName string    `json:"endpoint_name"`
+	EventType    *string   `json:"event_type"`
+	ReceivedAt   time.Time `json:"received_at"`
+}
 
 func (cfg *apiConfig) handlerListEvents(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	if r.Method != http.MethodGet {
-		respondWithError(w, http.StatusMethodNotAllowed, "Method not allowed", nil)
+		respondWithError(w, http.StatusMethodNotAllowed,
+			"Method not allowed", nil)
 		return
 	}
 
-	var events []database.WebhookEvent
+	var events []database.ListEventsRow
 	events, err := cfg.db.ListEvents(r.Context())
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "failed to list events", err)
+		respondWithError(w, http.StatusInternalServerError,
+			"failed to list events", err)
 		return
 	}
 
-	respondWithJSON(w, http.StatusOK, events)
+	responseEvents := []Event{}
+
+	for _, event := range events {
+		var eventType *string
+		if event.EventType.Valid {
+			value := event.EventType.String
+			eventType = &value
+		}
+		responseEvents = append(responseEvents, Event{
+			ID:           event.ID,
+			EndpointName: event.EndpointName,
+			EventType:    eventType,
+			ReceivedAt:   event.ReceivedAt,
+		})
+	}
+
+	respondWithJSON(w, http.StatusOK, responseEvents)
 }
