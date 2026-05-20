35de4cf49760855d537cd95d9eaa5ee9d7d7c277
Camille Onoda <info@camilleonoda.com>
2026-05-20 10:21:07 +0900

    feat(api): add Delivery response struct for listing deliveries

--- handler_deliveries_get.go
+++ handler_deliveries_get.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
+	"time"
 
 	"github.com/CamilleOnoda/webhook-relay.git/internal/database"
+	"github.com/google/uuid"
 )
+
+type Delivery struct {
+	ID                 uuid.UUID     `json:"id"`
+	TargetUrl          string        `json:"target_url"`
+	Status             string        `json:"status"`
+	CreatedAt          time.Time     `json:"created_at"`
+	DeliveryDurationMs sql.NullInt32 `json:"delivery_duration_ms"`
+}
 
 func (cfg *apiConfig) handlerListDeliveries(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -19,5 +30,18 @@ func (cfg *apiConfig) handlerListDeliveries(w http.ResponseWriter, r *http.Reque
 		respondWithError(w, http.StatusInternalServerError, "failed to list deliveries", err)
 		return
 	}
-	respondWithJSON(w, http.StatusOK, deliveries)
+
+	responseDeliveries := []Delivery{}
+
+	for _, delivery := range deliveries {
+		responseDeliveries = append(responseDeliveries, Delivery{
+			ID:                 delivery.ID,
+			TargetUrl:          delivery.TargetUrl,
+			Status:             delivery.Status,
+			CreatedAt:          delivery.CreatedAt,
+			DeliveryDurationMs: delivery.DeliveryDurationMs,
+		})
+	}
+
+	respondWithJSON(w, http.StatusOK, responseDeliveries)
 }
