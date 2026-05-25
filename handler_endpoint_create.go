954e663666aac94fd15fc689da54f23acb772db8
Camille Onoda <info@camilleonoda.com>
2026-05-25 13:52:45 +0900

    refactor(api): add a nullable UserID field to the Endpoint struct

--- handler_endpoint_create.go
+++ handler_endpoint_create.go
@@ -12,14 +12,15 @@ import (
 )
 
 type Endpoint struct {
-	ID           uuid.UUID `json:"id"`
-	Name         string    `json:"name"`
-	TargetUrl    string    `json:"target_url"`
-	IsActive     bool      `json:"is_active"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
-	GeneratedURL string    `json:"generated_url"`
-	Description  *string   `json:"description,omitempty"`
+	ID           uuid.UUID  `json:"id"`
+	Name         string     `json:"name"`
+	TargetUrl    string     `json:"target_url"`
+	IsActive     bool       `json:"is_active"`
+	CreatedAt    time.Time  `json:"created_at"`
+	UpdatedAt    time.Time  `json:"updated_at"`
+	GeneratedURL string     `json:"generated_url"`
+	Description  *string    `json:"description,omitempty"`
+	UserID       *uuid.UUID `json:"user_id"`
 }
 
 func (cfg *apiConfig) handlerCreateEndpoint(w http.ResponseWriter, r *http.Request) {
@@ -30,10 +31,12 @@ func (cfg *apiConfig) handlerCreateEndpoint(w http.ResponseWriter, r *http.Reque
 	}
 
 	var req struct {
-		Name        string  `json:"name"`
-		TargetUrl   string  `json:"target_url"`
-		Description *string `json:"description,omitempty"`
+		Name        string     `json:"name"`
+		TargetUrl   string     `json:"target_url"`
+		Description *string    `json:"description,omitempty"`
+		UserID      *uuid.UUID `json:"user_id"`
 	}
+
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid request payload", err)
 		return
@@ -61,10 +64,18 @@ func (cfg *apiConfig) handlerCreateEndpoint(w http.ResponseWriter, r *http.Reque
 		description = sql.NullString{Valid: false}
 	}
 
+	var userID uuid.NullUUID
+	if req.UserID != nil {
+		userID = uuid.NullUUID{UUID: *req.UserID, Valid: true}
+	} else {
+		userID = uuid.NullUUID{Valid: false}
+	}
+
 	dbEndpoint, err := cfg.db.CreateEndpoint(r.Context(), database.CreateEndpointParams{
 		Name:        req.Name,
 		TargetUrl:   validURL.String(),
 		Description: description,
+		UserID:      userID,
 	})
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Failed to create endpoint in database", err)
