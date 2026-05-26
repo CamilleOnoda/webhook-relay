7f1631691545045448b4aac886ca40680da62ced
Camille Onoda <info@camilleonoda.com>
2026-05-26 15:23:25 +0900

    feat(handler): protect endpoint deletion by user ownership

--- handler_endpointByID_delete.go
+++ handler_endpointByID_delete.go
@@ -3,14 +3,34 @@ package main
 import (
 	"net/http"
 
+	"github.com/CamilleOnoda/webhook-relay.git/internal/auth"
+	"github.com/CamilleOnoda/webhook-relay.git/internal/database"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerDeleteEndpointByID(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	if r.Method != http.MethodDelete {
 		respondWithError(w, http.StatusMethodNotAllowed, "Method not allowed", nil)
 		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized,
+			"failed to get token", err)
+		return
+	}
+	userIDFromToken, err := auth.ValidateJWT(token, cfg.jwt_secret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized,
+			"failed to validate token", err)
+		return
+	}
+
+	userID := uuid.NullUUID{
+		UUID:  userIDFromToken,
+		Valid: true,
 	}
 
 	endpointID := r.PathValue("id")
@@ -19,13 +39,20 @@ func (cfg *apiConfig) handlerDeleteEndpointByID(w http.ResponseWriter, r *http.R
 		respondWithError(w, http.StatusBadRequest, "invalid UUID format", err)
 		return
 	}
-	_, err = cfg.db.GetEndpointByID(r.Context(), id)
+
+	_, err = cfg.db.GetEndpointByIDAndUserID(r.Context(), database.GetEndpointByIDAndUserIDParams{
+		ID:     id,
+		UserID: userID,
+	})
 	if err != nil {
-		w.WriteHeader(http.StatusForbidden)
-		w.Write([]byte("endpoint not found"))
+		respondWithError(w, http.StatusNotFound, "endpoint not found", err)
 		return
 	}
-	err = cfg.db.DeleteEndpointByID(r.Context(), id)
+
+	err = cfg.db.DeleteEndpointByIDAndUserID(r.Context(), database.DeleteEndpointByIDAndUserIDParams{
+		ID:     id,
+		UserID: userID,
+	})
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Failed to delete endpoint", err)
 		return
