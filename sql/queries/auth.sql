188cf5868a13245361a186fcd801450f37c75c12
Camille Onoda <info@camilleonoda.com>
2026-06-18 11:08:36 +0900

    refactor(auth): make refresh token query explicit

--- sql/queries/auth.sql
+++ sql/queries/auth.sql
@@ -6,5 +6,5 @@ SELECT users.*
 FROM refresh_tokens
 JOIN users ON refresh_tokens.user_id = users.id
 WHERE refresh_tokens.token = $1
-AND revoked_at IS NULL
-AND expires_at > NOW();
+AND refresh_tokens.revoked_at IS NULL
+AND refresh_tokens.expires_at > NOW();
